aef0c4876ed79850038606584440a5362b9675d1
brianbellini <brianbellini@me.com>
2020-01-25 16:06:08 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
 console.log("Up and running!");
-let cardOne = "queen";
+let cards = ["queen", "queen", "king", "king"];
+/*let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
-let cardFour = "king";
+let cardFour = "king";*/
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
